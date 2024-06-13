9fb82577e4f9377b0b4143064a31c38b109777b2
NataliiaBor1 <borisenko.nataliia1@gmail.com>
2024-06-13 01:15:17 -0700

    added event listener for click

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ console.log("hello");
 function play() {
     console.log("Square was clicked");
 }
+
+const squares = document.querySelectorAll('.square');
+
+for (const square of squares) {
+    square.addEventListener('click', play);
+}
