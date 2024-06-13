a2e36748c72ecdd6465a3184a0b840967fadaa72
NataliiaBor1 <borisenko.nataliia1@gmail.com>
2024-06-13 01:27:04 -0700

    play function was refactored

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 console.log("hello");
 
+let player = 'X';
 function play() {
-    console.log("Square was clicked");
+    // Switch turns (ex.: if it was X's turn, it should be O's turn next)
+    if (player === 'X') {
+        player = 'O';
+    } else {
+        player = 'X';
+    }
+    // player = player === 'X' ? 'O' : 'X';
+    const playerSpan = document.querySelector('#current-player');
+    playerSpan.innerText = player;
 }
 
 const squares = document.querySelectorAll('.square');
